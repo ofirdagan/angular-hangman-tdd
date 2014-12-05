c8d0db89ff46e4460efe2c6bcb221a88712762d1
Ofir Dagan <ofirdagan@gmail.com>
2014-12-05 20:08:37 +0200

    feat(wordBank): remove ng-improved-testing

--- test/spec/services/word-bank.spec.js
+++ test/spec/services/word-bank.spec.js
@@ -1,51 +1,47 @@
 'use strict';
-/* global ModuleBuilder */
 
 describe('Service: wordBank', function () {
 
   var $q;
   var wordsMock = ['Terminator', 'Alian', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-  beforeEach(ModuleBuilder.forModule('hangmanAppInternal')
-    .serviceWithMocksFor('wordBank', 'hangmanApi')
-    .build());
-
   beforeEach(function () {
     module('testUtils');
+    module('hangmanAppInternal');
   });
 
   // instantiate service
   var wordBank;
-  beforeEach(inject(function (_wordBank_, _$q_, hangmanApiMock, testUtils) {
-    hangmanApiMock.getWordsInCategory = testUtils.anAsyncFunc();
+  beforeEach(inject(function (_wordBank_, _$q_, hangmanApi, testUtils) {
+    hangmanApi.getWordsInCategory = testUtils.anAsyncFunc();
     wordBank = _wordBank_;
     $q = _$q_;
   }));
 
-  it('should call server api in case it does not have any words in the category yet', inject(function ($rootScope, hangmanApiMock) {
+  it('should call server api in case it does not have any words in the category yet', inject(function ($rootScope, hangmanApi) {
     var category = 'movies';
     wordBank.getNextWord(category);
     $rootScope.$digest();
-    expect(hangmanApiMock.getWordsInCategory).toHaveBeenCalledWith(category);
+    expect(hangmanApi.getWordsInCategory).toHaveBeenCalledWith(category);
   }));
 
-  it('should return a word according to a category when no cache', inject(function (hangmanApiMock) {
+  it('should return a word according to a category when no cache', inject(function (hangmanApi) {
     var category = 'movies';
     var word = '';
 
     wordBank.getNextWord(category).then(function (randomWord) {
       word = randomWord;
     });
-    hangmanApiMock.getWordsInCategory.returns(wordsMock);
+    hangmanApi.getWordsInCategory.returns(wordsMock);
 
     expect(wordsMock.indexOf(word) !== -1).toBe(true);
   }));
 
-  it('should return a word according to a category when word bank is cached', inject(function ($rootScope, hangmanApiMock) {
+  it('should return a word according to a category when word bank is cached', inject(function ($rootScope, hangmanApi) {
     var category = 'movies';
     //build the cache
     wordBank.getNextWord(category);
-    hangmanApiMock.getWordsInCategory.returns(wordsMock);
+    hangmanApi.getWordsInCategory.returns(wordsMock);
 
     //ask for it another time
     var wasHereSpy = jasmine.createSpy('washere');
@@ -57,11 +53,11 @@ describe('Service: wordBank', function () {
 
     //make sure the word got from the cache and not from the server.
     expect(wasHereSpy).toHaveBeenCalled();
-    expect(hangmanApiMock.getWordsInCategory).toHaveBeenCalledOnce();
+    expect(hangmanApi.getWordsInCategory).toHaveBeenCalledOnce();
 
   }));
 
-  it('should not return same word twice', inject(function ($rootScope, hangmanApiMock) {
+  it('should not return same word twice', inject(function ($rootScope, hangmanApi) {
     var category = 'movies';
     var usedWords = [];
     var wasHereSpy = jasmine.createSpy('washere');
@@ -74,13 +70,13 @@ describe('Service: wordBank', function () {
 
     for (var i = 0; i < wordsMock.length; i++) {
       wordBank.getNextWord(category).then(onWordDrawn);
-      hangmanApiMock.getWordsInCategory.returns(wordsMock);
+      hangmanApi.getWordsInCategory.returns(wordsMock);
     }
 
     expect(wasHereSpy.calls.length).toBe(wordsMock.length);
   }));
 
-  it('should reset random index in case all of the words have been drawn', inject(function ($rootScope, hangmanApiMock) {
+  it('should reset random index in case all of the words have been drawn', inject(function ($rootScope, hangmanApi) {
     var lastWord;
     var wasHereSpy = jasmine.createSpy('washere');
 
@@ -93,7 +89,7 @@ describe('Service: wordBank', function () {
     _(200).times(function () {
       wordsMock.forEach(function () {
         wordBank.getNextWord().then(onWordDrawn);
-        hangmanApiMock.getWordsInCategory.returns(wordsMock);
+        hangmanApi.getWordsInCategory.returns(wordsMock);
       });
       lastWord = undefined;
       expect(wasHereSpy.calls.length).toBe(wordsMock.length);
