6cc83643c245ac1e893794947ba22a6f8201b953
ofird <ofirdagan@gmail.com>
2014-12-20 14:59:18 +0200

    fix(wix-angular): remove dependcy of wix-angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,4 @@ angular
 //load only the internal module in tests and mock any module dependency
 //the only exception to load this module in tests in to test the config & run blocks
 angular
-  .module('hangmanApp', ['hangmanAppInternal', 'hangmanTranslations', 'wixAngular', 'mobile-angular-ui']);
+  .module('hangmanApp', ['hangmanAppInternal', 'hangmanTranslations', 'mobile-angular-ui']);
